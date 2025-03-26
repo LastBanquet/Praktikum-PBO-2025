94a01e0fd4565614bed8dbf6d817daa324588358
LouisFerdian <ferdianlouisa@gmail.com>
2025-03-27 01:04:14 +0700

    Update Main.java

--- Week 4/105223055_LOUISA-THT-4/Main.java
+++ Week 4/105223055_LOUISA-THT-4/Main.java
@@ -1,3 +1,5 @@
+Scanner input = new Scanner(System.in);
+
 public class Main {
     public static void main(String[] args) {
         Produk prd1 = new Produk("01", "Laptop", 1500000, 10);
@@ -6,6 +8,10 @@ public class Main {
         System.out.println("Info Awal Sebelum Transaksi");
         prd1.info();
         plg1.info();
+
+        System.out.print("\nMasukkan jumlah top-up saldo: Rp ");
+        double jumlahTopUp = input.nextDouble();
+        plg1.topUp(jumlahTopUp); 
 
         Transaksi t1 = new Transaksi("01", plg1, prd1, 1);
         t1.proses();
