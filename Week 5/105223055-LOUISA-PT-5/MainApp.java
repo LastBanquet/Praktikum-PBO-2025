55448acd8f729e5a45166136500e66fc98a04bee
LouisFerdian <ferdianlouisa@gmail.com>
2025-03-28 23:57:29 +0700

    Update MainApp.java

--- Week 5/105223055-LOUISA-PT-5/MainApp.java
+++ Week 5/105223055-LOUISA-PT-5/MainApp.java
@@ -10,28 +10,28 @@ class Person {
     public void display() {
         System.out.println("Name: " + name + ", Age: " + age);
     }
-}
+} //Revisi 1, Menambahkan tutup kurawal untuk menutup class Person
 
 class Employee extends Person {
     private double salary;
 
     public Employee(String n, int a, double s) {
-        super(n, a); // Revisi 1, memanggil constructor Person dengan super() agar artibut name dan age terinisialisasi.
+        super(n, a); // Revisi 2, memanggil constructor Person dengan super() agar artibut name dan age terinisialisasi.
         salary = s;
     }
 
     public void showSalary() {
         System.out.println("Salary: " + salary);
-        //^Revisi 2 Mengganti koma (,) dengan titik koma (;)
+        //^Revisi 3, Mengganti koma (,) dengan titik koma (;)
     }
 }
 
 public class MainApp {
     public static void main(String[] args) {
         Employee emp = new Employee("Alice", 25, 5000.0);
-        //^Revisi 3 Mengubah 5.000 menjadi 5000.0 agar sesuai dengan format angka dalam tipe data double
+        //^Revisi 4, Mengubah 5.000 menjadi 5000.0 agar sesuai dengan format angka dalam tipe data double
         emp.display();
         emp.showSalary();
-        //Revisi 4 menambahkan dan memanggil showSalary {} supaya gaji juga dicetak
+        //Revisi 5, Memanggil method showSalary {} supaya gaji juga dicetak
     }
 }
